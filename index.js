8b84224c81fcac0c72d288936fd5f74d9df222b5
Fahsina24 <fahsinahossain10@gmail.com>
2025-02-22 23:40:54 +0000

    Basic server side setup is completed.

---
+++ index.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_PARCEL}:${process.env.DB_PASS}@cluster0.ymvbd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const userCollection = client.db("parcelDB").collection("users");
+    // save or update users
+    app.post("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = req.body;
+      console.log(user);
+      const existUser = await userCollection.findOne(query);
+      if (existUser) {
+        console.log("user exists before");
+        return res.send(existUser);
+      } else {
+        const result = await userCollection.insertOne({
+          ...user,
+          role: "user",
+        });
+        console.log(result);
+        return res.send(result);
+      }
+    });
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(3000, () => {
+  console.log("application is running");
+});
